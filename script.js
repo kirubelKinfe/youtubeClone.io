c08bced51ff1cc8a5f28229287c32cccb5755b98
kirubelKinfe <kirubelkinfe01@gmail.com>
2022-06-16 00:54:05 +0300

    Structuring the css folder

--- script.js
+++ script.js
@@ -9,26 +9,34 @@ menuIcon.onclick = function() {
 
 
 
-const videos = [{
-  title: 'ማለዳ | Maleda (Official Music Video)',
-  author: 'Kassmasse ካሥማሠ',
-  status: "1,887,633 views Premiered Jan 15, 2021",
-  id: 'id1'
+const videos = [
+  {
+    thumbnail: "thumbnails/thumbnail-7.webp",
+    channelIcon: "channel-pictures/channels4_profile.jpg",
+    title: "ማለዳ | Maleda (Official Music Video)",
+    author: "Kassmasse ካሥማሠ",
+    status: "1,887,633 views Premiered Jan 15, 2021",
+    id: "id1",
   },
   {
-    title: 'ES6 Tutorial: Learn Modern JavaScript in 1 Hour',
-    author: 'Programming with Mosh',
+    thumbnail: "thumbnails/thumbnail-3.webp",
+    channelIcon: "channel-pictures/channel-5.jpeg",
+    title: "ES6 Tutorial: Learn Modern JavaScript in 1 Hour",
+    author: "Programming with Mosh",
     status: "690k views 3 years ago",
-    id: 'id2'
+    id: "id2",
   },
   {
-    title: 'Junior Vs Senior Code - How To Write Better Code',
-    author: 'Web Dev Simplified',
+    thumbnail: "thumbnails/thumbnail-6.webp",
+    channelIcon: "channel-pictures/channel-2.jpeg",
+    title: "Junior Vs Senior Code - How To Write Better Code",
+    author: "Web Dev Simplified",
     status: "1M views 2 years ago",
-    id: 'id3'
-  }];
+    id: "id3",
+  },
+];
     
-console.log(videos[0].title)
+
 
     //View
     function render() {
@@ -37,9 +45,9 @@ console.log(videos[0].title)
         const element = document.createElement('div');
         element.classList = 'video-preview';
 
-        element.innerHTML = `<a id="vd3" href="playVideo.html"><img src="thumbnails/thumbnail-7.webp" class="thumbnail"></a>
+        element.innerHTML = `<a id="vd3" href="playVideo.html"><img src="${video.thumbnail}" class="thumbnail"></a>
       <div class="video">
-        <img src="channel-pictures/channels4_profile.jpg" alt="" class="channel-icon">
+        <img src="${video.channelIcon}" alt="" class="channel-icon">
         <div id="video-details" class="video-details">
           <a id="vd3" href="playVideo.html">
             <div class="video-title">${video.title}</div>
